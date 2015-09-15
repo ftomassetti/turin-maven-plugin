b642ebb87206e7665cad38151a0c3b19afb7989b
Federico Tomassetti <federico@tomassetti.me>
2015-09-15 23:47:20 +0200

    consider validation while compiling

--- src/main/java/me/tomassetti/turin/maven/TurinCompileMojo.java
+++ src/main/java/me/tomassetti/turin/maven/TurinCompileMojo.java
@@ -3,11 +3,13 @@ package me.tomassetti.turin.maven;
 import com.google.common.collect.ImmutableList;
 import me.tomassetti.turin.compiler.ClassFileDefinition;
 import me.tomassetti.turin.compiler.Compiler;
+import me.tomassetti.turin.compiler.errorhandling.ErrorCollector;
 import me.tomassetti.turin.parser.Parser;
 import me.tomassetti.turin.parser.analysis.resolvers.ComposedResolver;
 import me.tomassetti.turin.parser.analysis.resolvers.InFileResolver;
 import me.tomassetti.turin.parser.analysis.resolvers.Resolver;
 import me.tomassetti.turin.parser.analysis.resolvers.SrcResolver;
+import me.tomassetti.turin.parser.ast.Position;
 import me.tomassetti.turin.parser.ast.TurinFile;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -24,6 +26,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Compile Turin files
@@ -78,7 +81,7 @@ public class TurinCompileMojo extends AbstractMojo
         Parser parser = new Parser();
 
         // First we collect all TurinFiles and we pass it to the resolver
-        List<TurinFile> turinFiles = new ArrayList<>();
+        List<Parser.TurinFileAndFile> turinFiles = new ArrayList<>();
         for (File sourceDir : getTurinSourceDirs()) {
             try {
                 turinFiles.addAll(parser.parseAllIn(sourceDir));
@@ -94,14 +97,20 @@ public class TurinCompileMojo extends AbstractMojo
                 throw new MojoFailureException(message);
             }
         }
-        Resolver resolver = new ComposedResolver(ImmutableList.of(new InFileResolver(), new SrcResolver(turinFiles)));
+        Resolver resolver = new ComposedResolver(ImmutableList.of(new InFileResolver(), new SrcResolver(turinFiles.stream().map((tf)->tf.getTurinFile()).collect(Collectors.toList()))));
 
         // Then we compile all files
         // TODO consider classpath
         Compiler instance = new Compiler(resolver, new Compiler.Options());
-        for (TurinFile turinFile : turinFiles) {
+        for (Parser.TurinFileAndFile turinFile : turinFiles) {
+            ErrorCollector errorCollector = new ErrorCollector() {
+                @Override
+                public void recordSemanticError(Position position, String s) {
+                    getLog().error("[" + turinFile.getFile().getPath()+"] Error at "+position + " : " + s);
+                }
+            };
             try {
-                for (ClassFileDefinition classFileDefinition : instance.compile(turinFile)) {
+                for (ClassFileDefinition classFileDefinition : instance.compile(turinFile.getTurinFile(), errorCollector)) {
                     saveClassFile(classFileDefinition);
                 }
             } catch (RuntimeException e){
